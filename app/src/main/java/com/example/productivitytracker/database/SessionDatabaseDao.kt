e4933cf70bc357af4852df513f89dc301466f399
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-14 20:28:36 -0500

    implemented basic database dao functions

--- app/src/main/java/com/example/productivitytracker/database/SessionDatabaseDao.kt
+++ app/src/main/java/com/example/productivitytracker/database/SessionDatabaseDao.kt
@@ -1,4 +1,27 @@
 package com.example.productivitytracker.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+
+@Dao
 interface SessionDatabaseDao {
+
+    @Insert
+    fun insert(session: ProductiveSession)
+
+    @Update
+    fun update(session: ProductiveSession)
+
+    @Query("SELECT * FROM productive_session_table WHERE sessionId = :key")
+    fun getSession(key: Long): ProductiveSession
+
+    @Query("SELECT * FROM productive_session_table")
+    fun getAllSessions(): LiveData<List<ProductiveSession>>
+
+    @Query("SELECT * FROM productive_session_table ORDER BY sessionId DESC LIMIT 1")
+    fun getMostRecentSession(): ProductiveSession?
 }
