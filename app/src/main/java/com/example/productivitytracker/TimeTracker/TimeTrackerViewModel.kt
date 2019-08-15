2a59a51d783c3dc926fb07ed8cfe3822e3cfa570
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-14 20:20:27 -0500

    initial commit

---
+++ app/src/main/java/com/example/productivitytracker/TimeTracker/TimeTrackerViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.productivitytracker.TimeTracker
+
+//fun onClickStart() {
+//    TODO()
+//}
+//
+//fun onClickStop() {
+//    TODO()
+//}
+//
+//fun onClickClear() {
+//    TODO()
+//}
