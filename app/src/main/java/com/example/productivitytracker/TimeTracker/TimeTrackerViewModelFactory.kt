e77cc5ae148af31bf53244e62c431957f14b44ca
clarkmyfancy <clark.jack0@gmail.com>
2019-08-15 11:29:39 -0500

    implemented factory

---
+++ app/src/main/java/com/example/productivitytracker/TimeTracker/TimeTrackerViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.example.productivitytracker.TimeTracker
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.productivitytracker.database.SessionDatabaseDao
+
+class TimeTrackerViewModelFactory (
+        private val dataSource: SessionDatabaseDao,
+        private val application: Application) : ViewModelProvider.Factory {
+    
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TimeTrackerViewModel::class.java)) {
+            return TimeTrackerViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
