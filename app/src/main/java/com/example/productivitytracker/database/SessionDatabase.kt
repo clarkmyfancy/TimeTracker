f736fe5c870c1430b0bce58dd4ffe999ac6bdb66
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-14 21:39:42 -0500

    implemented abstract singleton database class with companion object

--- app/src/main/java/com/example/productivitytracker/database/SessionDatabase.kt
+++ app/src/main/java/com/example/productivitytracker/database/SessionDatabase.kt
@@ -1,2 +1,38 @@
 package com.example.productivitytracker.database
 
+import android.content.Context
+import android.se.omapi.Session
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ProductiveSession::class], version = 1, exportSchema = false)
+abstract class SessionDatabase : RoomDatabase() {
+
+    abstract val sessionDatabaseDao: SessionDatabaseDao
+
+    // allows clients to access properties and methods of class without instantiating it
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SessionDatabase? = null
+
+        fun getInstance(context: Context): SessionDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SessionDatabase::class.java,
+                        "productive_session_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
